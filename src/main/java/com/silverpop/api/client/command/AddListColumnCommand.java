ff3f8191dc1d3e5cb21d83e1c2369a3422951c6d
rkovarik <roman.kovarik@magnolia-cms.com>
2015-10-21 11:37:36 +0200

    Issue #38 correct listId annotation

--- src/main/java/com/silverpop/api/client/command/AddListColumnCommand.java
+++ src/main/java/com/silverpop/api/client/command/AddListColumnCommand.java
@@ -10,7 +10,7 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
 @XmlApiProperties("AddListColumn")
 public class AddListColumnCommand implements ApiCommand {
 
-    @XStreamAlias("VISIBILITY")
+    @XStreamAlias("LIST_ID")
     private Integer listId;
 
     @XStreamAlias("COLUMN_TYPE")
@@ -42,6 +42,19 @@ public class AddListColumnCommand implements ApiCommand {
         return columnType;
     }
 
+    /**
+     * 0 – Text column
+     * 1 – YES/No column
+     * 2 – Numeric column
+     * 3 – Date column
+     * 4 – Time column
+     * 5 – Country column
+     * 6 – Select one
+     * 8 – Segmenting
+     * 15 – SMS Phone Number 16 – Phone Number
+     * 17 – Timestamp
+     * 20 – Multi-Select
+     */
     public void setColumnType(Integer columnType) {
         this.columnType = columnType;
     }
